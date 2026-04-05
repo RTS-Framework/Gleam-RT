f12b9038dec5b55b143e7b9fc2f951382a9eaf52
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-05 12:35:10 +0800

    opts_stub.asm: remove the comment about structure, use INCLUDE <inst/option.inst>.

--- asm/opts_stub.asm
+++ asm/opts_stub.asm
@@ -2,20 +2,6 @@ IFDEF _WIN32
 .model flat
 ENDIF
 
-; ===================================== Runtime Options =====================================
-;
-; uint64 ShieldModuleHash;    // set the target module base address.
-; uint64 ShieldEntryPoint;    // define the RVA of the shield.
-; bool   EnableSecurityMode;  // detect environment when initialize runtime.
-; bool   DisableDetector;     // disable detector for test or debug.
-; bool   DisableSysmon;       // disable sysmon for implement single thread model.
-; bool   DisableWatchdog;     // disable watchdog for implement single thread model.
-; bool   NotEraseInstruction; // not erase runtime instructions after call Runtime_M.Exit
-; bool   NotAdjustProtect;    // not adjust current memory page protect for erase runtime
-; bool   TrackCurrentThread;  // track current thread for some special executable file like Go
-;
-; ===========================================================================================
-
 .code
 
 IFDEF _WIN32
@@ -24,29 +10,7 @@ ELSE
   Option_Stub proc
 ENDIF
 
-  ; 0xFC is magic for valid
-  ; then append options
-  db 0FCh, 000h, 000h, 000h
-
-  ; reversed and 16 bytes aligned
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
-  db 000h, 000h, 000h, 000h
+INCLUDE <inst/option.inst>
 
 IFDEF _WIN32
   _Option_Stub@0 endp
