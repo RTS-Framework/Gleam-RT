a519f19cf07df27923cafc19be4cba18fda1790a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-06 13:13:30 +0800

    shield.h: add comment about stub structure, add extern void Shield_Stub.

--- include/shield.h
+++ include/shield.h
@@ -5,6 +5,16 @@
 #include "win_types.h"
 #include "dll_kernel32.h"
 
+// +------------+---------+-------------+--------+------------+-------+
+// | magic mark | xor key | shield size | shield | decoy size | decoy |
+// +------------+---------+-------------+--------+------------+-------+
+// |    0xFB    | 32 byte |   uint16    |   var  |   uint16   |  var  |
+// +------------+---------+-------------+--------+------------+-------+
+
+void Shield_Sleep(DWORD dwMilliseconds);
+
+void Shield_Exit();
+
 typedef struct {
     uintptr BeginAddress;
     uintptr EndAddress;
@@ -15,5 +25,11 @@ typedef struct {
 } Shield_Ctx;
 
 bool DefenseRT(Shield_Ctx* ctx);
+
+// reserve stub for store shield and decoy
+#pragma warning(push)
+#pragma warning(disable : 4276)
+extern void Shield_Stub(void* args);
+#pragma warning(pop)
 
 #endif // SHIELD_H
