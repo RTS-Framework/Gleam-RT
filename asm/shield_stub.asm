2b888217dd42ad912c26022ca8d753d1a5f6ea6d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-06 12:23:14 +0800

    shield_stub.asm: add shield stub for store the generated shield and decoy.

---
+++ asm/shield_stub.asm
@@ -0,0 +1,17 @@
+IFDEF _WIN32
+.model flat
+ENDIF
+
+.code
+
+IFDEF _WIN32
+  _Shield_Stub@4 proc
+  INCLUDE <inst/shield_x86.inst>
+  _Shield_Stub@4 endp
+ELSE
+  Shield_Stub proc
+  INCLUDE <inst/shield_x64.inst>
+  Shield_Stub endp
+ENDIF
+
+end
