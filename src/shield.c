fa718b0a5964e6c6f281ef82fc85de8bb251b8c7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-16 13:36:11 +0800

    shield.c: add typedef struct Shield, implement InitShield, add internal methods.

--- src/shield.c
+++ src/shield.c
@@ -3,22 +3,32 @@
 #include "dll_kernel32.h"
 #include "lib_memory.h"
 #include "rel_addr.h"
+#include "hash_api.h"
+#include "random.h"
+#include "errno.h"
+#include "context.h"
+#include "layout.h"
 #include "shield.h"
+#include "debug.h"
+
+#define METHOD_SLEEP 1
+#define METHOD_STOP  2
 
 typedef struct {
     uintptr Method;
 
     VirtualProtect_t      VirtualProtect;
     WaitForSingleObject_t WaitForSingleObject;
+    uintptr               Reserved;
 
-    uintptr Reserved;
-    void*   CriticalAddr;
-    uint    CriticalSize;
-    void*   DecoyAddr;
-    uint    DecoySize;
-    void*   Shelter;
-    HANDLE  hTimer;
-} sleep_args;
+    void* CriticalAddr;
+    uint  CriticalSize;
+    void* DecoyAddr;
+    uint  DecoySize;
+
+    void*  Shelter;
+    HANDLE hTimer;
+} Sleep_Args;
 
 typedef struct {
     uintptr Method;
@@ -31,7 +41,104 @@ typedef struct {
     uint  CriticalSize;
     void* DecoyAddr;
     uint  DecoySize;
-} exit_Args;
+} Stop_Args;
+
+typedef struct {
+    // store options
+    bool NotEraseInstruction;
+    bool NotAdjustProtect;
+
+    VirtualProtect_t      VirtualProtect;
+    VirtualFree_t         VirtualFree;
+    ExitThread_t          ExitThread;
+    WaitForSingleObject_t WaitForSingleObject;
+
+    // global mutex
+    HANDLE hMutex;
+
+    SD_Status status;
+} Shield;
+
+// methods for user
+BOOL SD_GetStatus(SD_Status* status);
+
+// methods for runtime
+bool  SD_Lock();
+bool  SD_Unlock();
+void  SD_Sleep(DWORD dwMilliseconds);
+void  SD_Stop();
+errno SD_Clean();
+
+// hard encoded address in getShieldPointer for replacement
+#ifdef _WIN64
+    #define SHIELD_POINTER 0x7FABCDEF111111FF
+#elif _WIN32
+    #define SHIELD_POINTER 0x7FABCDFF
+#endif
+static Shield* getShieldPointer();
+
+static bool initShieldAPI(Shield* shield, Context* context);
+static bool updateShieldPointer(Shield* shield);
+static bool recoverShieldPointer(Shield* shield);
+static bool initShieldEnvironment(Shield* shield, Context* context);
+static void eraseShieldMethods(Context* context);
+static void cleanShield(Shield* shield);
+
+Shield_M* InitShield(Context* context)
+{
+    // set structure address
+    uintptr addr = context->MainMemPage;
+    uintptr shieldAddr = addr + LAYOUT_SD_STRUCT + RandUintN(addr, 128);
+    uintptr methodAddr = addr + LAYOUT_SD_METHOD + RandUintN(addr, 128);
+    // allocate shield memory
+    Shield* shield = (Shield*)shieldAddr;
+    mem_init(shield, sizeof(Shield));
+    // store options
+    shield->NotEraseInstruction = context->NotEraseInstruction;
+    shield->NotAdjustProtect    = context->NotAdjustProtect;
+    // initialize shield
+    errno errno = NO_ERROR;
+    for (;;)
+    {
+        if (!initShieldAPI(shield, context))
+        {
+            errno = ERR_SHIELD_INIT_API;
+            break;
+        }
+        if (!updateShieldPointer(shield))
+        {
+            errno = ERR_SHIELD_UPDATE_PTR;
+            break;
+        }
+        if (!initShieldEnvironment(shield, context))
+        {
+            errno = ERR_SHIELD_INIT_ENV;
+            break;
+        }
+        break;
+    }
+    eraseShieldMethods(context);
+    if (errno != NO_ERROR)
+    {
+        SetLastErrno(errno);
+        return NULL;
+    }
+    // create methods for shield
+    Shield_M* method = (Shield_M*)methodAddr;
+    // methods for user
+    method->GetStatus = GetFuncAddr(&SD_GetStatus);
+    // methods for runtime
+    method->Lock   = GetFuncAddr(&SD_Lock);
+    method->Unlock = GetFuncAddr(&SD_Unlock);
+    method->Sleep  = GetFuncAddr(&SD_Sleep);
+    method->Stop   = GetFuncAddr(&SD_Stop);
+    method->Clean  = GetFuncAddr(&SD_Clean);
+    return method;
+}
+
+
+
+
 
 // Only the instructions related to the DefenseRT function are
 // in plain text during Sleep, so if you need to advance AV, 
