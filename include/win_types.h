10aee05ab32ca787cd65b848616f0d9aedf48675
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-07 15:28:23 +0800

    win_types.h: add more basic typedef about Windows for PEB and TEB.

--- include/win_types.h
+++ include/win_types.h
@@ -3,29 +3,42 @@
 
 #include "c_types.h"
 
+typedef int32 BOOL;
+typedef uint8 BOOLEAN;
+
 typedef uint8  BYTE;
 typedef uint16 WORD;
 typedef uint32 DWORD;
 typedef uint64 QWORD;
 
-typedef int8  CHAR;
-typedef int16 SHORT;
-typedef int32 LONG;
-typedef int64 LONGLONG;
+typedef int8   CHAR;
+typedef int16  SHORT;
+typedef int32  LONG;
+typedef int64  LONGLONG;
+typedef uint16 USHORT;
+typedef uint32 ULONG;
+typedef uint64 ULONGLONG;
 
-typedef int32 BOOL;
-typedef uint  UINT;
-typedef uint  SIZE_T;
-typedef uint  ULONG_PTR;
+typedef uint UINT;
+typedef uint SIZE_T;
+typedef uint ULONG_PTR;
 
 typedef void* POINTER;
-typedef void* PVOID;
 typedef void* HANDLE;
+
+typedef void*   PVOID;
+typedef uint8*  PSTR;
+typedef uint16* PWSTR;
+typedef HANDLE* PHANDLE;
 
 typedef void*   LPVOID;
 typedef uint8*  LPSTR;
 typedef uint16* LPWSTR;
 typedef HANDLE* LPHANDLE;
+
+typedef const void*   PCVOID;
+typedef const uint8*  PCSTR;
+typedef const uint16* PCWSTR;
 
 typedef const void*   LPCVOID;
 typedef const uint8*  LPCSTR;
