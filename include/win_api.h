4693dc7dde0a40c23e73a5956f0ff4cc276c127c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-07 15:21:40 +0800

    win_api.h: prepare typedef struct about PEB, TEB and InMemoryOrderModuleList.

--- include/win_api.h
+++ include/win_api.h
@@ -5,6 +5,65 @@
 #include "win_types.h"
 #include "dll_kernel32.h"
 
+typedef struct {
+    struct LIST_ENTRY* Flink;
+    struct LIST_ENTRY* Blink;
+} LIST_ENTRY;
+
+typedef struct {
+    WORD  Length;
+    WORD  MaximumLength;
+    PWSTR Buffer;
+} UNICODE_STRING;
+
+typedef struct {
+    DWORD  Length;
+    BOOL   Initialized;
+    HANDLE SsHandle;
+
+    LIST_ENTRY InLoadOrderModuleList;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+} PEB_LDR_DATA;
+
+typedef struct {
+    LIST_ENTRY InLoadOrderLinks;
+    LIST_ENTRY InMemoryOrderLinks;
+    LIST_ENTRY InInitializationOrderLinks;
+
+    PVOID DllBase;
+    PVOID EntryPoint;
+    DWORD SizeOfImage;
+
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+} LDR_DATA_TABLE_ENTRY;
+
+typedef struct {
+    bool InheritedAddressSpace;
+    bool ReadImageFileExecOptions;
+    bool BeingDebugged;
+    bool SpareBool;
+
+    HANDLE Mutant;
+    PVOID  ImageBaseAddress;
+
+    PEB_LDR_DATA* Ldr;
+} PEB;
+
+typedef struct {
+    PVOID NtTib;
+    PVOID EnvironmentPointer;
+    PVOID ExceptionList;
+    PVOID StackBase;
+    PVOID StackLimit;
+    PVOID SubSystemTib;
+    PVOID FiberData;
+    PVOID ArbitraryUserPointer;
+    struct TEB* Self;
+    PVOID EnvironmentBlock;
+} TEB;
+
 DWORD   GetModuleFileName(void* list, HMODULE hModule, LPWSTR lpFilename, DWORD nSize);
 HMODULE GetModuleHandle(void* list, LPWSTR lpFilename);
 
