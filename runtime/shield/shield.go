8a1c99fc6a8a763638dc06a36b0f7395e7827687
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-09 12:38:04 +0800

    shield/shield.go: add Status struct that contains shield status, add GetStatus.

---
+++ runtime/shield/shield.go
@@ -0,0 +1,44 @@
+//go:build windows
+
+package shield
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+
+	"github.com/RTS-Framework/GRT-Develop/metric"
+)
+
+var (
+	modGleamRT = windows.NewLazyDLL("GleamRT.dll")
+
+	procStatus = modGleamRT.NewProc("SD_Status")
+)
+
+// Status contains sysmon status.
+type Status struct {
+	EntryPoint    uintptr `json:"entry_point"`
+	BaseAddress   uintptr `json:"base_address"`
+	IsPreInjected bool    `json:"is_pre_injected"`
+	IsAllocated   bool    `json:"is_allocated"`
+}
+
+// GetStatus is used to get sysmon status.
+func GetStatus() (*Status, error) {
+	var status metric.SDStatus
+	ret, _, err := procStatus.Call(uintptr(unsafe.Pointer(&status))) // #nosec
+	if ret == 0 {
+		en := uintptr(err.(syscall.Errno))
+		return nil, fmt.Errorf("failed to call shield.Status: 0x%08X", en)
+	}
+	s := Status{
+		EntryPoint:    status.EntryPoint,
+		BaseAddress:   status.BaseAddress,
+		IsPreInjected: status.IsPreInjected.ToBool(),
+		IsAllocated:   status.IsAllocated.ToBool(),
+	}
+	return &s, nil
+}
