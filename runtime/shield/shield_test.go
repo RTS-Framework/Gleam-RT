3ccc00ea8aa71334521ff90bcff3fc7a27f12a66
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-09 12:38:44 +0800

    shield/shield_test.go: add TestMain and TestGetStatus for dump status.

---
+++ runtime/shield/shield_test.go
@@ -0,0 +1,74 @@
+//go:build windows
+
+package shield
+
+import (
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows"
+
+	"github.com/RTS-Framework/Gleam-RT/runtime"
+)
+
+func init() {
+	var src string
+	switch runtime.GOARCH {
+	case "386":
+		src = "../../dist/GleamRT_x86.dll"
+	case "amd64":
+		src = "../../dist/GleamRT_x64.dll"
+	}
+	dll, err := os.ReadFile(src)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("GleamRT.dll", dll, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	err := gleamrt.Initialize(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+
+	err = gleamrt.Uninitialize()
+	if err != nil {
+		panic(err)
+	}
+
+	// must free twice for runtime package
+	err = windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+	err = windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Remove("GleamRT.dll")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(code)
+}
+
+func TestGetStatus(t *testing.T) {
+	status, err := GetStatus()
+	require.NoError(t, err)
+
+	require.False(t, status.IsPreInjected)
+	require.True(t, status.IsAllocated)
+
+	spew.Dump(status)
+}
