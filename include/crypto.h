602d256be1871fa47845a5aa51823312fd392051
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-18 15:14:12 +0800

    crypto.h: add EraseBuffer for erase data in buffer, it not free memory.

--- include/crypto.h
+++ include/crypto.h
@@ -6,13 +6,16 @@
 #define CRYPTO_IV_SIZE  16
 #define CRYPTO_KEY_SIZE 32
 
-// EncryptBuf is used to encrypt data in buffer with 256 bit key.
-void EncryptBuf(void* buf, uint size, byte* key, byte* iv);
+// EncryptBuffer is used to encrypt data in buffer with 256 bit key.
+void EncryptBuffer(void* buf, uint size, byte* key, byte* iv);
 
-// DecryptBuf is used to decrypt data in buffer with 256 bit key.
-void DecryptBuf(void* buf, uint size, byte* key, byte* iv);
+// DecryptBuffer is used to decrypt data in buffer with 256 bit key.
+void DecryptBuffer(void* buf, uint size, byte* key, byte* iv);
 
-// XORBuf is used to xor data in buffer with key.
-void XORBuf(void* buf, uint bufSize, void* key, uint keySize);
+// XORBuffer is used to xor data in buffer with key.
+void XORBuffer(void* buf, uint bufSize, void* key, uint keySize);
+
+// EraseBuffer is used to erase data in buffer, it not free memory.
+void EraseBuffer(void* buf, uint size);
 
 #endif // CRYPTO_H
