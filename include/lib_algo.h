7076ee54312e1d46f958eb85bd80937b03e405a1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-20 14:12:33 +0800

    lib_algo.h: add a library header file for define standard algorithm.

---
+++ include/lib_algo.h
@@ -0,0 +1,17 @@
+#ifndef LIB_ALGO_H
+#define LIB_ALGO_H
+
+#include "c_types.h"
+
+typedef struct {
+    uint32 state[8];
+    uint64 bitlen;
+    uint8  data[64];
+    uint8  datalen;
+} SHA256_Ctx;
+
+void SHA256_Init(SHA256_Ctx* ctx);
+void SHA256_Write(SHA256_Ctx* ctx, void* data, uint len);
+void SHA256_Sum(SHA256_Ctx* ctx, byte (*hash)[32]);
+
+#endif // LIB_ALGO_H
