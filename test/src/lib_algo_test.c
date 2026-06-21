90f1a275a5996c89f0e24510d7fc1a954955c1cb
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-21 14:11:14 +0800

    src/lib_algo_test.c: implement TestLibAlgo_SHA256 about TestLibAlgo.

--- test/src/lib_algo_test.c
+++ test/src/lib_algo_test.c
@@ -27,12 +27,28 @@ bool TestLibAlgo()
 
 static bool TestLibAlgo_SHA256()
 {
-    byte s[] = { 1, 2, 3, 4 };
+    byte data[] = { 1, 2, 3, 4 };
 
-    // small
+    SHA256_Ctx ctx;
+    byte  hash[32];
+    SHA256_Init(&ctx);
+    for (int i = 0; i < 1000; i++)
+    {
+        SHA256_Write(&ctx, data, sizeof(data));
+    }
+    SHA256_Sum(&ctx, &hash);
 
-    // large
-
+    byte expected[] = {
+        0xCA, 0x01, 0xCB, 0x69, 0x1B, 0xF1, 0xD6, 0xB4, 
+        0xB1, 0x87, 0x2A, 0x7F, 0xF2, 0xFE, 0xFF, 0xA5, 
+        0xD3, 0xC9, 0x53, 0xA2, 0xFA, 0x7E, 0x6E, 0x37, 
+        0x4C, 0xCB, 0x28, 0xEF, 0x7E, 0x07, 0x57, 0xF0,
+    };
+    if (!mem_equal(expected, hash, sizeof(hash)))
+    {
+        printf_s("invalid SHA256 digest\n");
+        return false;
+    }
 
     printf_s("test SHA256 passed\n");
     return true;
