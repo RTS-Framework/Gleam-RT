ed50df7fe165d30c19f5f24394da5d9d7d24e82b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-23 12:34:38 +0800

    lib_encode.c: implement the methods about Hex that from old file.

---
+++ src/lib_encode.c
@@ -0,0 +1,64 @@
+#include "c_types.h"
+#include "lib_encode.h"
+
+static byte Hex_byte(byte b);
+static int  Hex_value(byte s);
+
+uint Hex_Encode(void* src, uint len, byte* dst)
+{
+    byte* buf = (byte*)src;
+    for (uint i = 0; i < len; i++)
+    {
+        byte b = buf[i];
+        dst[i * 2 + 0] = Hex_byte(b >> 4);
+        dst[i * 2 + 1] = Hex_byte(b >> 0);
+    }
+    return len * 2;
+}
+
+static byte Hex_byte(byte v)
+{
+    v &= 0x0F;
+    if (v < 10)
+    {
+        return '0' + v;
+    }
+    return 'A' + (v - 10);
+}
+
+uint Hex_Decode(byte* src, uint len, void* dst)
+{
+    if (len & 1)
+    {
+        return 0;
+    }
+    byte* buf = (byte*)dst;
+    for (uint i = 0; i < len; i += 2)
+    {
+        int h = Hex_value(src[i + 0]);
+        int l = Hex_value(src[i + 1]);
+        if (h < 0 || l < 0)
+        {
+            return 0;
+        }
+        buf[i / 2] = (byte)(h * 16 + l);
+    }
+    return len / 2;
+}
+
+static int Hex_value(byte s)
+{
+    if (s >= '0' && s <= '9')
+    {
+        return s - '0';
+    }
+    if (s >= 'a' && s <= 'f')
+    {
+        return s - 'a' + 10;
+    }
+    if (s >= 'A' && s <= 'F')
+    {
+        return s - 'A' + 10;
+    }
+    return -1;
+}
