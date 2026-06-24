4edf32d19fde0df293c00938263a32296d1bfdcf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-24 13:39:29 +0800

    lib_encode.h: add comment for hex encoding, add methods about base64 encoding.

--- include/lib_encode.h
+++ include/lib_encode.h
@@ -3,7 +3,16 @@
 
 #include "c_types.h"
 
+// methods about hex encoding.
+// if dst is NULL, it only calculate the output length.
+// it will return -1 when call Decode with invalid data.
 uint Hex_Encode(void* src, uint len, byte* dst);
 uint Hex_Decode(byte* src, uint len, void* dst);
+
+// methods about base64 encoding.
+// if dst is NULL, it only calculate the output length.
+// it will return -1 when call Decode with invalid data.
+uint Base64_Encode(void* src, uint len, byte* dst);
+uint Base64_Decode(byte* src, uint len, void* dst);
 
 #endif // LIB_ENCODE_H
