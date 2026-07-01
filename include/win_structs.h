bab458da4a98e8bef75763e4bbef1569fe05c24a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-01 12:25:43 +0800

    win_structs.h: rename the header file for filename style, update include guard.

---
+++ include/win_structs.h
@@ -0,0 +1,111 @@
+#ifndef WIN_STRUCTS_H
+#define WIN_STRUCTS_H
+
+#include "c_types.h"
+#include "win_types.h"
+
+typedef struct {
+    struct LIST_ENTRY* Flink;
+    struct LIST_ENTRY* Blink;
+} LIST_ENTRY;
+
+typedef struct {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING;
+
+typedef struct {
+    ULONG  Length;
+    BOOL   Initialized;
+    HANDLE SsHandle;
+
+    LIST_ENTRY InLoadOrderModuleList;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+} PEB_LDR_DATA;
+
+typedef struct {
+    LIST_ENTRY InLoadOrderLinks;
+    LIST_ENTRY InMemoryOrderLinks;
+    LIST_ENTRY InInitializationOrderLinks;
+
+    PVOID DllBase;
+    PVOID EntryPoint;
+    ULONG SizeOfImage;
+
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+} LDR_DATA_TABLE_ENTRY;
+
+typedef struct {
+    BOOLEAN InheritedAddressSpace;
+    BOOLEAN ReadImageFileExecOptions;
+    BOOLEAN BeingDebugged;
+
+    UCHAR ImageUsedLargePages          : 1;
+    UCHAR IsProtectedProcess           : 1;
+    UCHAR IsImageDynamicallyRelocated  : 1;
+    UCHAR SkipPatchingUser32Forwarders : 1;
+    UCHAR IsPackagedProcess            : 1;
+    UCHAR IsAppContainer               : 1;
+    UCHAR IsProtectedProcessLight      : 1;
+    UCHAR IsLongPathAwareProcess       : 1;
+
+    HANDLE Mutant;
+    PVOID  ImageBaseAddress;
+
+    PEB_LDR_DATA* LDR;
+
+    PVOID  ProcessParameters;
+    PVOID  SubSystemData;
+    HANDLE ProcessHeap;
+    PVOID  FastPEBLock;
+    PVOID  AtlThunkSListPtr;
+    PVOID  IFEOKey;
+
+    ULONG ProcessInJob               : 1;
+    ULONG ProcessInitializing        : 1;
+    ULONG ProcessUsingVEH            : 1;
+    ULONG ProcessUsingVCH            : 1;
+    ULONG ProcessUsingFTH            : 1;
+    ULONG ProcessPreviouslyThrottled : 1;
+    ULONG ProcessCurrentlyThrottled  : 1;
+    ULONG ProcessImagesHotPatched    : 1;
+    ULONG ReservedBits0              : 24;
+
+    PVOID KernelCallbackTable;
+    ULONG Reserved;
+    ULONG AtlThunkSListPtr32;
+    PVOID ApiSetMap;
+} PEB;
+
+typedef struct {
+    PVOID ExceptionList;
+	PVOID StackBase;
+	PVOID StackLimit;
+	PVOID SubSystemTIB;
+    PVOID FiberData;
+	PVOID ArbitraryUserPointer;
+    struct NT_TIB* Self;
+} NT_TIB;
+
+typedef struct {
+    HANDLE UniqueProcess;
+    HANDLE UniqueThread;
+} CLIENT_ID;
+
+typedef struct {
+    NT_TIB    TIB;
+    PVOID     EnvironmentPointer;
+    CLIENT_ID ClientID;
+    PVOID     ActiveRpcHandle;
+    PVOID     ThreadLocalStoragePointer;
+    PEB*      ProcessEnvironmentBlock;
+    ULONG     LastErrorValue;
+    ULONG     CountOfOwnedCriticalSections;
+    PVOID     CsrClientThread;
+    PVOID     Win32ThreadInfo;
+} TEB;
+
+#endif // WIN_STRUCTS_H
