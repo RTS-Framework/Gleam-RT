de5ecf3a908a5de69d2574c6b9d832a5162b54d2
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-04 13:34:23 +0800

    hash_api.h: redesign PML interface, add GetDefaultPML and comment.

--- include/hash_api.h
+++ include/hash_api.h
@@ -7,54 +7,66 @@
 
 // Process Module List (PML).
 //
-// Runtime abstraction of a process module entry.
+// Runtime abstraction of a process module.
 //
-// Can be populated from any module source, including:
-//   - InLoadOrderLinks
-//   - InMemoryOrderLinks
-//   - InInitializationOrderLinks
+// The runtime consumes PML as the canonical module representation.
+// By default, PML instances are backed by InMemoryOrderModuleList,
+// providing real-time synchronization with the Windows loader.
+// Alternative providers may construct PML entries from custom module
+// sources when required.
+//
+// PML can be populated from various module providers, including:
+//   - InMemoryOrderModuleList (native Windows loader)
 //   - Runtime-generated module snapshots
-//   - Custom PE loaders
 //   - Other module enumeration methods
+//   - Custom PE loaders
 //
 // The Links field represents the active list linkage for the selected
-// module source and is not tied to a specific Windows loader list.
-typedef struct {                
-    LIST_ENTRY Links;           
+// provider and is not tied to any specific Windows loader list.
+//
+// Layout-compatible with the leading fields of LDR_DATA_TABLE_ENTRY.
+typedef struct {
+    LIST_ENTRY Padding0;
+    LIST_ENTRY Links;
+    LIST_ENTRY Padding1;
 
     PVOID DllBase;
     PVOID EntryPoint;
     ULONG SizeOfImage;
-    ULONG Reserved;
 
     UNICODE_STRING FullDllName;
     UNICODE_STRING BaseDllName;
 } PML;
 
-// FindAPI will not call GetProcAddress, if this module is
-// not loaded, it cannot find the target procedure address.
+// FindAPI will not call GetProcAddress, if this module is not loaded,
+// it cannot find the target procedure address.
 //
-// FindAPI is support forwarded procedure.
+// FindAPI is support forwarded exports.
 // FindAPI is NOT support DLL about API Sets.
 
-typedef void* (*FindAPI_t)(uint module, uint procedure, uint key);
-typedef void* (*FindAPI_ML_t)(void* list, uint module, uint procedure, uint key);
+// MH means with module hash. MA means with module address(HMODULE).
+typedef void* (*FindAPI_MH_t)(uint  module, uint procedure, uint key);
+typedef void* (*FindAPI_MA_t)(void* module, uint procedure, uint key);
+
+// M*L means operate on a caller-provided PML provider.
+typedef void* (*FindAPI_MHL_t)(PML* list, uint  module, uint procedure, uint key);
+typedef void* (*FindAPI_MAL_t)(PML* list, void* module, uint procedure, uint key);
+
+// shortcut for debug, test and toolchain.
 typedef void* (*FindAPI_A_t)(byte* module, byte* procedure);
 typedef void* (*FindAPI_W_t)(uint16* module, byte* procedure);
 
-// FindAPI is used to find Windows API address by hash and key.
-void* FindAPI(uint module, uint procedure, uint key);
+// find procedure address with hash and key.
+void* FindAPI_MH(uint  module, uint procedure, uint key);
+void* FindAPI_MA(void* module, uint procedure, uint key);
 
-// FindAPI_ML is used to find Windows API address by hash and key.
-// But it will use the cached InMemoryOrderModuleList.
-void* FindAPI_ML(void* list, uint module, uint procedure, uint key);
+// find procedure address with custom PML.
+void* FindAPI_MHL(PML* list, uint  module, uint procedure, uint key);
+void* FindAPI_MAL(PML* list, void* module, uint procedure, uint key);
 
-// FindAPI_A is used to find Windows API address by module name
-// and procedure name with ANSI, it is a wrapper about FindAPI.
+// FindAPI_A/W is used to find Windows API address by module name and
+// procedure name with ANSI/UTF-16, it is a wrapper about FindAPI_Mx.
 void* FindAPI_A(byte* module, byte* procedure);
-
-// FindAPI_W is used to find Windows API address by module name
-// and procedure name with UTF-16, it is a wrapper about FindAPI.
 void* FindAPI_W(uint16* module, byte* procedure);
 
 // CalcModHash_A is used to calculate module ANSI name hash with key.
@@ -72,9 +84,7 @@ uint   CalcProcHash  (byte* procedure, uint key);
 uint32 CalcProcHash32(byte* procedure, uint32 key);
 uint64 CalcProcHash64(byte* procedure, uint64 key);
 
-// methods about get process module list.
-void* GetInLoadOrderModuleList();
-void* GetInMemoryOrderModuleList();
-void* GetInInitializationOrderModuleList();
+// get default process module list.
+PML* GetDefaultPML();
 
 #endif // HASH_API_H
