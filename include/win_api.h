e2c19c014288762ba8b3dc3d9febc1eec0f15609
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-09 15:24:04 +0800

    win_api.h: add IsValidModuleHandle, refactor GetModuleBaseNameW and GetModuleHandleW.

--- include/win_api.h
+++ include/win_api.h
@@ -4,8 +4,15 @@
 #include "c_types.h"
 #include "win_types.h"
 #include "dll_kernel32.h"
+#include "hash_api.h"
 
-DWORD   GetModuleFileName(void* list, HMODULE hModule, LPWSTR lpFilename, DWORD nSize);
-HMODULE GetModuleHandle(void* list, LPWSTR lpFilename);
+// IsValidModuleHandle is used to check module handle is valid.
+BOOL IsValidModuleHandle(PML* pml, HMODULE hModule);
+
+// GetModuleBaseNameW is used to get module base name by handle.
+DWORD GetModuleBaseNameW(PML* pml, HMODULE hModule, PWSTR lpBasename, DWORD nSize);
+
+// GetModuleHandleW is used to get module handle by base name.
+HMODULE GetModuleHandleW(PML* pml, PWSTR lpBasename);
 
 #endif // WIN_API_H
