9b9ed1123378380ae0c17c86bb1a620d05e8cfe8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-09 13:16:35 +0800

    layout.h: extend main memory page size for store more module and data page.

--- include/layout.h
+++ include/layout.h
@@ -1,88 +1,93 @@
 #ifndef LAYOUT_H
 #define LAYOUT_H
 
-// +--------------+-------------+------------+-------------------+---------------+
-// |    0-4096    |  4096-8192  | 8192-20480 |    20480-32768    |  32768-40960  |
-// +--------------+-------------+------------+-------------------+---------------+
-// | runtime core | base module | submodules | high-level module | suffix module | 
-// +--------------+-------------+------------+-------------------+---------------+
+// +--------------+--------------+-------------+--------------------+---------------+-------------+
+// |    0-8192    |  8192-16384  | 16384-32768 |    32768-49152     |  49152-53248  | 53248-65536 |
+// +--------------+--------------+-------------+--------------------+---------------+-------------+
+// | runtime core | base modules | sub modules | high-level modules | suffix module |  data page  |
+// +--------------+--------------+-------------+--------------------+---------------+-------------+
 
-// the main memory page store the data of all the 
-// core structures, like runtime, submodules...
+// the main memory page store the data of all the core structures.
+// data page is used to store the decrypted constant for modules.
 
-#define MAIN_MEM_PAGE_SIZE (10 * 4096)
+#define MAIN_MEM_PAGE_SIZE (64 * 1024)
 
-// ----------runtime core-----------
+// ------------runtime core------------
 
-#define LAYOUT_RUNTIME_STRUCT 256
-#define LAYOUT_RUNTIME_MODULE 2560
+#define LAYOUT_RUNTIME_STRUCT 1024
+#define LAYOUT_RUNTIME_MODULE 4096
 
-// -----------base module-----------
+// ------------base modules------------
 
-// Spoof Call & Indirect Syscall
-#define LAYOUT_SI_STRUCT 4096
-#define LAYOUT_SI_MODULE 5000
+// Spoof Call
+#define LAYOUT_SC_STRUCT 8192
+#define LAYOUT_SC_MODULE 9000
 
-// Detector
-#define LAYOUT_DT_STRUCT 6000
-#define LAYOUT_DT_MODULE 7000
+// Indirect Syscall
+#define LAYOUT_SS_STRUCT 10000
+#define LAYOUT_SS_MODULE 11000
 
-// ------------submodule------------
+#define LAYOUT_DT_STRUCT 12000
+#define LAYOUT_DT_MODULE 13000
+
+// -------------sub modules------------
 
 // Library Tracker
-#define LAYOUT_LT_STRUCT 8192
-#define LAYOUT_LT_MODULE 9000
+#define LAYOUT_LT_STRUCT 16384
+#define LAYOUT_LT_MODULE 17000
 
 // Memory Tracker
-#define LAYOUT_MT_STRUCT 10000
-#define LAYOUT_MT_MODULE 11000
+#define LAYOUT_MT_STRUCT 18000
+#define LAYOUT_MT_MODULE 19000
 
 // Thread Tracker
-#define LAYOUT_TT_STRUCT 12000
-#define LAYOUT_TT_MODULE 13000
+#define LAYOUT_TT_STRUCT 21000
+#define LAYOUT_TT_MODULE 22000
 
 // Resource Tracker
-#define LAYOUT_RT_STRUCT 14000
-#define LAYOUT_RT_MODULE 15000
+#define LAYOUT_RT_STRUCT 23000
+#define LAYOUT_RT_MODULE 24000
 
 // Argument Store
-#define LAYOUT_AS_STRUCT 16000
-#define LAYOUT_AS_MODULE 16500
+#define LAYOUT_AS_STRUCT 26000
+#define LAYOUT_AS_MODULE 26500
 
 // In-memory Storage
-#define LAYOUT_IS_STRUCT 17000
-#define LAYOUT_IS_MODULE 17500
+#define LAYOUT_IS_STRUCT 27000
+#define LAYOUT_IS_MODULE 27500
 
-// --------high-level module--------
+// ---------high-level modules---------
 
 // WinBase
-#define LAYOUT_WB_STRUCT 20480
-#define LAYOUT_WB_METHOD 21000
+#define LAYOUT_WB_STRUCT 32768
+#define LAYOUT_WB_METHOD 34500
 
 // WinFile
-#define LAYOUT_WF_STRUCT 22000
-#define LAYOUT_WF_METHOD 23000
+#define LAYOUT_WF_STRUCT 36000
+#define LAYOUT_WF_METHOD 37000
 
 // WinHTTP
-#define LAYOUT_WH_STRUCT 24000
-#define LAYOUT_WH_METHOD 25000
+#define LAYOUT_WH_STRUCT 38000
+#define LAYOUT_WH_METHOD 39000
 
 // WinCrypto
-#define LAYOUT_WC_STRUCT 26000
-#define LAYOUT_WC_METHOD 27000
+#define LAYOUT_WC_STRUCT 40000
+#define LAYOUT_WC_METHOD 41000
 
 // Watchdog
-#define LAYOUT_WD_STRUCT 28000
-#define LAYOUT_WD_METHOD 28500
+#define LAYOUT_WD_STRUCT 42000
+#define LAYOUT_WD_METHOD 43000
 
 // Sysmon
-#define LAYOUT_SM_STRUCT 29000
-#define LAYOUT_SM_METHOD 29500
+#define LAYOUT_SM_STRUCT 44000
+#define LAYOUT_SM_METHOD 45000
 
-// ----------suffix module----------
+// -----------suffix modules-----------
 
 // Shield
-#define LAYOUT_SD_STRUCT 32768
-#define LAYOUT_SD_METHOD 33500
+#define LAYOUT_SD_STRUCT 49152
+#define LAYOUT_SD_METHOD 50000
+
+// -------------data page--------------
 
 #endif // LAYOUT_H
