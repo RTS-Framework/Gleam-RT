ff58b08e13562e57fa69faa6dc2375c677c573c7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-10 12:23:24 +0800

    win_api.c: improve GetModuleBaseNameW and GetModuleHandleW.

--- src/win_api.c
+++ src/win_api.c
@@ -1,5 +1,6 @@
 #include "c_types.h"
 #include "win_types.h"
+#include "win_structs.h"
 #include "dll_kernel32.h"
 #include "lib_memory.h"
 #include "lib_string.h"
@@ -24,8 +25,12 @@ BOOL IsValidModuleHandle(PML* pml, HMODULE hModule)
     return false;
 }
 
-DWORD GetModuleBaseNameW(PML* pml, HMODULE hModule, PWSTR lpBasename, DWORD nSize)
+DWORD GetModuleBaseNameW(PML* pml, HMODULE hModule, LPWSTR lpBasename, DWORD nSize)
 {
+    if (hModule == NULL || lpBasename == NULL || nSize == 0)
+    {
+        return 0;
+    }
     LIST_ENTRY* head = &pml->Links;
     for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
     {
@@ -34,30 +39,44 @@ DWORD GetModuleBaseNameW(PML* pml, HMODULE hModule, PWSTR lpBasename, DWORD nSiz
         {
             continue;
         }
-        PWSTR  nameBuf = entry->BaseName.Buffer;
-        USHORT nameLen = entry->BaseName.Length;
-        if (nameLen > nSize*2)
+        LPWSTR nameBuf = entry->BaseName.Buffer;
+        USHORT nameLen = entry->BaseName.Length/2;
+        if (nameLen > nSize)
         {
-            nameLen = (USHORT)(nSize*2);
+            nameLen = (USHORT)nSize;
         }
-        mem_copy(lpBasename, nameBuf, nameLen);
+        strncpy_w(lpBasename, nameBuf, nameLen);
+        lpBasename[nameLen] = 0x0000;
         return nameLen;
     }
     return 0;
 }
 
-HMODULE GetModuleHandleW(PML* pml, PWSTR lpBasename)
+HMODULE GetModuleHandleW(PML* pml, LPWSTR lpBasename)
 {
+    if (lpBasename == NULL)
+    {
+        return NULL;
+    }
     LIST_ENTRY* head = &pml->Links;
     for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
     {
         PML* entry = (PML*)((uintptr)(link)-offsetof(PML, Links));
-        PWSTR  nameBuf = entry->BaseName.Buffer;
+        LPWSTR nameBuf = entry->BaseName.Buffer;
         USHORT nameLen = entry->BaseName.Length/2;
+        if (strlen_w(lpBasename) != nameLen)
+        {
+            continue;
+        }
         if (strnicmp_w(lpBasename, nameBuf, nameLen) == 0)
         {
             return entry->ImageBase;
         }
     }
     return NULL;
+}
+
+BOOL IsDebuggerPresent(PEB* peb)
+{
+    return peb->BeingDebugged;
 }
