db38834e4989b0107c73b64e60e4ae2bb2bb657c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-10 12:21:25 +0800

    win_api.h: add IsDebuggerPresent, adjust the type of arguments.

--- include/win_api.h
+++ include/win_api.h
@@ -3,6 +3,7 @@
 
 #include "c_types.h"
 #include "win_types.h"
+#include "win_structs.h"
 #include "dll_kernel32.h"
 #include "hash_api.h"
 
@@ -10,9 +11,12 @@
 BOOL IsValidModuleHandle(PML* pml, HMODULE hModule);
 
 // GetModuleBaseNameW is used to get module base name by handle.
-DWORD GetModuleBaseNameW(PML* pml, HMODULE hModule, PWSTR lpBasename, DWORD nSize);
+DWORD GetModuleBaseNameW(PML* pml, HMODULE hModule, LPWSTR lpBasename, DWORD nSize);
 
 // GetModuleHandleW is used to get module handle by base name.
-HMODULE GetModuleHandleW(PML* pml, PWSTR lpBasename);
+HMODULE GetModuleHandleW(PML* pml, LPWSTR lpBasename);
+
+// IsDebuggerPresent is used to check current process is being debugged.
+BOOL IsDebuggerPresent(PEB* peb);
 
 #endif // WIN_API_H
