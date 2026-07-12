d30742a5d58c9d14498c96e47cb85843daf46b38
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-12 15:27:33 +0800

    pe_image.c: improve ParsePEImage about search the first executable section.

--- src/pe_image.c
+++ src/pe_image.c
@@ -1,9 +1,15 @@
 #include "c_types.h"
 #include "win_types.h"
-#include "lib_string.h"
-#include "crypto.h"
 #include "pe_image.h"
 
+// not search the section that named ".text".
+//
+// this is more robust than checking for the section name ".text," because
+// the code section of some PE files (such as those generated via packers,
+// LLVM, Clang, or custom linker scripts) is not necessarily named ".text,"
+// whereas an executable section will always carry the `IMAGE_SCN_MEM_EXECUTE`.
+//
+// another benefit is that the ".text" string does not appear in the runtime.
 void ParsePEImage(void* address, PE_Image* image)
 {
     uintptr imageAddr = (uintptr)address;
@@ -12,30 +18,22 @@ void ParsePEImage(void* address, PE_Image* image)
     Image_NTHeaders*      ntHeaders  = (Image_NTHeaders*)(imageAddr + hdrOffset);
     Image_FileHeader*     fileHeader = &ntHeaders->FileHeader;
     Image_OptionalHeader* optHeader  = &ntHeaders->OptionalHeader;
-    // not record the original ".text" bytes
-    byte target[] = {
-        '.'^0x19, 't'^0xF4, 'e'^0xBF, 'x'^0x8C,
-        't'^0x19, 000^0xF4, 000^0xBF, 000^0x8C,
-    };
-    byte key[] = { 0x19, 0xF4, 0xBF, 0x8C };
-    XORBuffer(target, sizeof(target), key, sizeof(key));
     // get address of first section header
     uintptr fileAddr = imageAddr + hdrOffset + sizeof(ntHeaders->Signature);
     uintptr optAddr  = fileAddr + sizeof(Image_FileHeader);
     uint32  optSize  = fileHeader->SizeOfOptionalHeader;
+    // search the first executable section
     Image_SectionHeader* section = (Image_SectionHeader*)(optAddr + optSize);
-    // search .text section
     for (uint16 i = 0; i < fileHeader->NumberOfSections; i++)
     {
-        if (strncmp_a((ANSI)section, (ANSI)target, sizeof(target)) != 0)
+        if (section->Characteristics & IMAGE_SCN_MEM_EXECUTE)
         {
-            section++;
-            continue;
+            image->Text = *section;
+            break;
         }
-        image->Text = *section;
-        break;
+        section++;
     }
-    // store parse result
+    // store the parsed result
     image->EntryPoint     = imageAddr + optHeader->AddressOfEntryPoint;
     image->ImageBase      = optHeader->ImageBase;
     image->ImageSize      = optHeader->SizeOfImage;
