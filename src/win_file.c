d7cef55bcad042b2e749052fb7c3efbcdf3691db
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-16 12:48:21 +0800

    win_file.c: refactor setModulePointer, update InitWinFile and WF_Uninstall.

--- src/win_file.c
+++ src/win_file.c
@@ -3,12 +3,14 @@
 #include "win_types.h"
 #include "dll_kernel32.h"
 #include "lib_memory.h"
-#include "rel_addr.h"
 #include "hash_api.h"
+#include "rel_addr.h"
 #include "random.h"
+#include "crypto.h"
 #include "errno.h"
 #include "context.h"
 #include "layout.h"
+#include "ptr_table.h"
 #include "win_file.h"
 #include "debug.h"
 
@@ -19,10 +21,10 @@
 #endif
 
 typedef struct {
-    // store options
+    // store option
     bool NotEraseInstruction;
 
-    // API addresses
+    // API address
     CreateFileA_t   CreateFileA;
     CreateFileW_t   CreateFileW;
     GetFileSizeEx_t GetFileSizeEx;
@@ -44,19 +46,12 @@ errno WF_WriteFileW(LPWSTR path, databuf* file);
 // methods for runtime
 errno WF_Uninstall();
 
-// hard encoded address in getModulePointer for replacement
-#ifdef _WIN64
-    #define MODULE_POINTER 0x7FABCDEF111111E2
-#elif _WIN32
-    #define MODULE_POINTER 0x7FABCDE2
-#endif
 static WinFile* getModulePointer();
 
 static bool initModuleAPI(WinFile* module, Context* context);
-static bool updateModulePointer(WinFile* module);
-static bool recoverModulePointer(WinFile* module);
-static bool initModuleEnvironment(WinFile* module, Context* context);
-static void eraseModuleMethods(Context* context);
+static bool initModuleEnv(WinFile* module, Context* context);
+static void eraseModuleMethod(Context* context);
+static void setModulePointer(WinFile* module);
 
 errno readFile(HANDLE hFile, databuf* file);
 errno writeFile(HANDLE hFile, databuf* file);
@@ -81,24 +76,20 @@ WinFile_M* InitWinFile(Context* context)
             errno = ERR_WIN_FILE_INIT_API;
             break;
         }
-        if (!updateModulePointer(module))
-        {
-            errno = ERR_WIN_FILE_UPDATE_PTR;
-            break;
-        }
-        if (!initModuleEnvironment(module, context))
+        if (!initModuleEnv(module, context))
         {
             errno = ERR_WIN_FILE_INIT_ENV;
             break;
         }
         break;
     }
-    eraseModuleMethods(context);
+    eraseModuleMethod(context);
     if (errno != NO_ERROR)
     {
         SetLastErrno(errno);
         return NULL;
     }
+    setModulePointer(module);
     // create method set
     WinFile_M* method = (WinFile_M*)methodAddr;
     method->ReadFileA  = GetFuncAddr(&WF_ReadFileA);
@@ -109,33 +100,34 @@ WinFile_M* InitWinFile(Context* context)
     return method;
 }
 
+__declspec(noinline)
 static bool initModuleAPI(WinFile* module, Context* context)
 {
-    typedef struct { 
-        uint mHash; uint pHash; uint hKey; void* proc;
+    typedef struct {
+        uint pHash; uint hKey; void* proc;
     } winapi;
     winapi list[] =
 #ifdef _WIN64
     {
-        { 0x317DD0F693475EA8, 0xB8051A7915B80944, 0x0F743D616B0CAEE6 }, // CreateFileA
-        { 0x93F5D06BE816866D, 0x3735E419DDE43453, 0xE7876845E2EEE5F9 }, // CreateFileW
-        { 0x21CE75D66E4090A8, 0x19FA8270E3357B8E, 0xB404659651609EFC }, // GetFileSizeEx
-        { 0x6FE4F6657EFBD93E, 0x897FBABEB06C235B, 0xB850B555B85E70FB }, // ReadFile
-        { 0xD75FA9589DD972CD, 0xB88F4B527CFE51D5, 0x9F87323E42C1C109 }, // WriteFile
+        { 0xB8051A7915B80944, 0x0F743D616B0CAEE6 }, // CreateFileA
+        { 0x3735E419DDE43453, 0xE7876845E2EEE5F9 }, // CreateFileW
+        { 0x19FA8270E3357B8E, 0xB404659651609EFC }, // GetFileSizeEx
+        { 0x897FBABEB06C235B, 0xB850B555B85E70FB }, // ReadFile
+        { 0xB88F4B527CFE51D5, 0x9F87323E42C1C109 }, // WriteFile
     };
 #elif _WIN32
     {
-        { 0x4A79EC7C, 0x041548A2, 0x6078A702 }, // CreateFileA
-        { 0x5CDE3B8B, 0xC144ECFB, 0x773BD184 }, // CreateFileW
-        { 0x5357BA7F, 0xE65C60FB, 0x1B4E1DC2 }, // GetFileSizeEx
-        { 0x306A983D, 0x9D073E4A, 0x3DA8E38C }, // ReadFile
-        { 0x81CB5D38, 0x16637045, 0x522192A1 }, // WriteFile
+        { 0x041548A2, 0x6078A702 }, // CreateFileA
+        { 0xC144ECFB, 0x773BD184 }, // CreateFileW
+        { 0xE65C60FB, 0x1B4E1DC2 }, // GetFileSizeEx
+        { 0x9D073E4A, 0x3DA8E38C }, // ReadFile
+        { 0x16637045, 0x522192A1 }, // WriteFile
     };
 #endif
     for (int i = 0; i < arrlen(list); i++)
     {
         winapi item = list[i];
-        void*  proc = context->FindAPI(item.mHash, item.pHash, item.hKey);
+        void*  proc = context->FindAPI_MA(context->hKernel32, item.pHash, item.hKey);
         if (proc == NULL)
         {
             return false;
@@ -152,76 +144,38 @@ static bool initModuleAPI(WinFile* module, Context* context)
     return true;
 }
 
-// CANNOT merge updateModulePointer and recoverModulePointer
-// to one function with two arguments, otherwise the compiler
-// will generate the incorrect instructions.
-
-static bool updateModulePointer(WinFile* module)
-{
-    bool success = false;
-    uintptr target = (uintptr)(GetFuncAddr(&getModulePointer));
-    for (uintptr i = 0; i < 64; i++)
-    {
-        uintptr* pointer = (uintptr*)(target);
-        if (*pointer != MODULE_POINTER)
-        {
-            target++;
-            continue;
-        }
-        *pointer = (uintptr)module;
-        success = true;
-        break;
-    }
-    return success;
-}
-
-static bool recoverModulePointer(WinFile* module)
-{
-    bool success = false;
-    uintptr target = (uintptr)(GetFuncAddr(&getModulePointer));
-    for (uintptr i = 0; i < 64; i++)
-    {
-        uintptr* pointer = (uintptr*)(target);
-        if (*pointer != (uintptr)module)
-        {
-            target++;
-            continue;
-        }
-        *pointer = MODULE_POINTER;
-        success = true;
-        break;
-    }
-    return success;
-}
-
-static bool initModuleEnvironment(WinFile* module, Context* context)
+__declspec(noinline)
+static bool initModuleEnv(WinFile* module, Context* context)
 {
     module->malloc = context->mt_malloc;
     module->free   = context->mt_free;
     return true;
 }
 
-static void eraseModuleMethods(Context* context)
+__declspec(noinline)
+static void eraseModuleMethod(Context* context)
 {
     if (context->NotEraseInstruction)
     {
         return;
     }
     uintptr begin = (uintptr)(GetFuncAddr(&initModuleAPI));
-    uintptr end   = (uintptr)(GetFuncAddr(&eraseModuleMethods));
+    uintptr end   = (uintptr)(GetFuncAddr(&eraseModuleMethod));
     uintptr size  = end - begin;
-    RandBuffer((byte*)begin, (int64)size);
+    EraseInstruction((void*)begin, size);
 }
 
-// updateModulePointer will replace hard encode address to the actual address.
-// Must disable compiler optimize, otherwise updateModulePointer will fail.
-#pragma optimize("", off)
+__declspec(noinline)
+static void setModulePointer(WinFile* module)
+{
+    *(WinFile**)(POINTER_OFFSET_WIN_FILE) = module;
+}
+
+__declspec(noinline)
 static WinFile* getModulePointer()
 {
-    uintptr pointer = MODULE_POINTER;
-    return (WinFile*)(pointer);
+    return *(WinFile**)POINTER_OFFSET_WIN_FILE;
 }
-#pragma optimize("", on)
 
 __declspec(noinline)
 errno WF_ReadFileA(LPSTR path, databuf* file)
@@ -229,7 +183,7 @@ errno WF_ReadFileA(LPSTR path, databuf* file)
     WinFile* module = getModulePointer();
 
     HANDLE hFile = module->CreateFileA(
-        path, GENERIC_READ, 0, NULL, 
+        path, GENERIC_READ, 0, NULL,
         OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
     );
     if (hFile == INVALID_HANDLE_VALUE)
@@ -245,7 +199,7 @@ errno WF_ReadFileW(LPWSTR path, databuf* file)
     WinFile* module = getModulePointer();
 
     HANDLE hFile = module->CreateFileW(
-        path, GENERIC_READ, 0, NULL, 
+        path, GENERIC_READ, 0, NULL,
         OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
     );
     if (hFile == INVALID_HANDLE_VALUE)
@@ -253,6 +207,38 @@ errno WF_ReadFileW(LPWSTR path, databuf* file)
         return GetLastErrno();
     }
     return readFile(hFile, file);
+}
+
+__declspec(noinline)
+errno WF_WriteFileA(LPSTR path, databuf* file)
+{
+    WinFile* module = getModulePointer();
+
+    HANDLE hFile = module->CreateFileA(
+        path, GENERIC_WRITE, 0, NULL,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+    );
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        return GetLastErrno();
+    }
+    return writeFile(hFile, file);
+}
+
+__declspec(noinline)
+errno WF_WriteFileW(LPWSTR path, databuf* file)
+{
+    WinFile* module = getModulePointer();
+
+    HANDLE hFile = module->CreateFileW(
+        path, GENERIC_WRITE, 0, NULL,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+    );
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        return GetLastErrno();
+    }
+    return writeFile(hFile, file);
 }
 
 __declspec(noinline)
@@ -330,38 +316,6 @@ errno readFile(HANDLE hFile, databuf* file)
 }
 
 __declspec(noinline)
-errno WF_WriteFileA(LPSTR path, databuf* file)
-{
-    WinFile* module = getModulePointer();
-
-    HANDLE hFile = module->CreateFileA(
-        path, GENERIC_WRITE, 0, NULL, 
-        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
-    );
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
-        return GetLastErrno();
-    }
-    return writeFile(hFile, file);
-}
-
-__declspec(noinline)
-errno WF_WriteFileW(LPWSTR path, databuf* file)
-{
-    WinFile* module = getModulePointer();
-
-    HANDLE hFile = module->CreateFileW(
-        path, GENERIC_WRITE, 0, NULL, 
-        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
-    );
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
-        return GetLastErrno();
-    }
-    return writeFile(hFile, file);
-}
-
-__declspec(noinline)
 errno writeFile(HANDLE hFile, databuf* file)
 {
     WinFile* module = getModulePointer();
@@ -405,17 +359,5 @@ errno writeFile(HANDLE hFile, databuf* file)
 __declspec(noinline)
 errno WF_Uninstall()
 {
-    WinFile* module = getModulePointer();
-
-    errno errno = NO_ERROR;
-
-    // recover instructions
-    if (module->NotEraseInstruction)
-    {
-        if (!recoverModulePointer(module) && errno == NO_ERROR)
-        {
-            errno = ERR_WIN_FILE_RECOVER_INST;
-        }
-    }
-    return errno;
+    return NO_ERROR;
 }
