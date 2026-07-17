e25304e78fe629b98f7fd07c598ba4acb5dafdf5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-17 13:56:41 +0800

    crypto.h: add ObfuscateBuffer and IlluminateBuffer, adjust method order.

--- include/crypto.h
+++ include/crypto.h
@@ -3,8 +3,8 @@
 
 #include "c_types.h"
 
-#define CRYPTO_IV_SIZE  16
 #define CRYPTO_KEY_SIZE 32
+#define CRYPTO_IV_SIZE  16
 
 // EncryptBuffer is used to encrypt data in buffer with 256 bit key.
 void EncryptBuffer(void* buf, uint size, byte* key, byte* iv);
@@ -12,8 +12,22 @@ void EncryptBuffer(void* buf, uint size, byte* key, byte* iv);
 // DecryptBuffer is used to decrypt data in buffer with 256 bit key.
 void DecryptBuffer(void* buf, uint size, byte* key, byte* iv);
 
+// ObfuscateBuffer is used to obfuscate memory data, it is used to
+// preserve statistical characteristics as much as possible.
+void ObfuscateBuffer(void* buf, uint size, uint64 key);
+
+// IlluminateBuffer is used to recover obfuscated memory data.
+void IlluminateBuffer(void* buf, uint size, uint64 key);
+
 // XORBuffer is used to xor data in buffer with key.
 void XORBuffer(void* buf, uint bufSize, void* key, uint keySize);
+
+// SubstituteBuffer is used to replace each byte in the buffer using
+// a randomly generated substitution table (S-box).
+void SubstituteBuffer(void* buf, uint size);
+
+// ShuffleBuffer is used to randomly permutes the order of bytes in the buffer.
+void ShuffleBuffer(void* buf, uint size);
 
 // EraseBuffer is used to erase data in buffer, it will not free memory.
 void EraseBuffer(void* buf, uint size);
@@ -21,12 +35,5 @@ void EraseBuffer(void* buf, uint size);
 // EraseInstruction is used to erase instruction, it is used to preserve
 // statistical characteristics as much as possible while erasing instructions.
 void EraseInstruction(void* buf, uint size);
-
-// ShuffleBuffer is used to randomly permutes the order of bytes in the buffer.
-void ShuffleBuffer(void* buf, uint size);
-
-// SubstituteBuffer is used to replace each byte in the buffer using
-// a randomly generated substitution table (S-box).
-void SubstituteBuffer(void* buf, uint size);
 
 #endif // CRYPTO_H
