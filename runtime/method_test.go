87b75a1999baa11c4d253311213ede44c46f8fe6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-19 13:22:27 +0800

    method_test.go: update tests about TestGetProcAddress, update TestGetPML.

--- runtime/method_test.go
+++ runtime/method_test.go
@@ -67,126 +67,59 @@ func TestInitialize(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestGetProcAddressByName(t *testing.T) {
-	libKernel32, err := windows.LoadLibrary("kernel32.dll")
+func TestGetProcAddress(t *testing.T) {
+	hKernel32, err := windows.LoadLibrary("kernel32.dll")
 	require.NoError(t, err)
-	hKernel32 := uintptr(libKernel32)
 
-	VirtualAlloc, err := windows.GetProcAddress(libKernel32, "VirtualAlloc")
+	VirtualAlloc, err := windows.GetProcAddress(hKernel32, "VirtualAlloc")
 	require.NoError(t, err)
 
 	t.Run("redirected", func(t *testing.T) {
-		proc, err := GetProcAddressByName(hKernel32, "VirtualAlloc", true)
+		proc, err := GetProcAddress(hKernel32, "VirtualAlloc")
 		require.NoError(t, err)
-		require.NotZero(t, proc)
 		require.NotEqual(t, VirtualAlloc, proc)
 	})
 
-	t.Run("not redirected", func(t *testing.T) {
-		proc, err := GetProcAddressByName(hKernel32, "VirtualAlloc", false)
-		require.NoError(t, err)
-		require.Equal(t, VirtualAlloc, proc)
-	})
-
-	t.Run("not found procedure", func(t *testing.T) {
-		proc, err := GetProcAddressByName(hKernel32, "NotFound", false)
-		require.EqualError(t, err, "failed to call GetProcAddressByName: 0x0000007F")
+	t.Run("procedure not found", func(t *testing.T) {
+		proc, err := GetProcAddress(hKernel32, "NotFound")
+		require.EqualError(t, err, "failed to call GetProcAddress: 0x0000007F")
 		require.Zero(t, proc)
 	})
 
-	t.Run("not found method", func(t *testing.T) {
-		proc, err := GetProcAddressByName(Handle, "NotFound", false)
-		require.EqualError(t, err, "failed to call GetProcAddressByName: 0xFF000304")
+	t.Run("method not found", func(t *testing.T) {
+		proc, err := GetProcAddress(Handle, "NotFound")
+		require.EqualError(t, err, "failed to call GetProcAddress: 0xFF000302")
 		require.Zero(t, proc)
 	})
 }
 
-func TestGetProcAddressByHash(t *testing.T) {
+func TestGetProcAddressEx(t *testing.T) {
 	hKernel32, err := windows.LoadLibrary("kernel32.dll")
 	require.NoError(t, err)
+
 	VirtualAlloc, err := windows.GetProcAddress(hKernel32, "VirtualAlloc")
 	require.NoError(t, err)
 
-	var (
-		mHash uint64
-		pHash uint64
-		hhKey uint64
-	)
-	switch runtime.GOARCH {
-	case "386":
-		mHash = 0xED38BE94
-		pHash = 0x2EC158C4
-		hhKey = 0xB33593DB
-	case "amd64":
-		mHash = 0x01D79EDD3081D078
-		pHash = 0x447B8E23EA19AFBF
-		hhKey = 0xC733FDBD9B57119F
-	}
-
 	t.Run("redirected", func(t *testing.T) {
-		proc, err := GetProcAddressByHash(uint(mHash), uint(pHash), uint(hhKey), true)
+		proc, err := GetProcAddressEx(hKernel32, "VirtualAlloc", true)
 		require.NoError(t, err)
-		require.NotZero(t, proc)
 		require.NotEqual(t, VirtualAlloc, proc)
 	})
 
 	t.Run("not redirected", func(t *testing.T) {
-		proc, err := GetProcAddressByHash(uint(mHash), uint(pHash), uint(hhKey), false)
+		proc, err := GetProcAddressEx(hKernel32, "VirtualAlloc", false)
 		require.NoError(t, err)
 		require.Equal(t, VirtualAlloc, proc)
 	})
 
 	t.Run("not found", func(t *testing.T) {
-		proc, err := GetProcAddressByHash(0x1111, 0x2222, 0x3333, false)
-		require.EqualError(t, err, "failed to call GetProcAddressByHash: 0xFF000303")
+		proc, err := GetProcAddressEx(hKernel32, "NotFound", false)
+		require.EqualError(t, err, "failed to call GetProcAddressEx: 0x0000007F")
 		require.Zero(t, proc)
 	})
 }
 
-func TestGetProcAddressByHashML(t *testing.T) {
-	hKernel32, err := windows.LoadLibrary("kernel32.dll")
-	require.NoError(t, err)
-	VirtualAlloc, err := windows.GetProcAddress(hKernel32, "VirtualAlloc")
-	require.NoError(t, err)
-
-	list := GetIMOML()
-	var (
-		mHash uint64
-		pHash uint64
-		hhKey uint64
-	)
-	switch runtime.GOARCH {
-	case "386":
-		mHash = 0xED38BE94
-		pHash = 0x2EC158C4
-		hhKey = 0xB33593DB
-	case "amd64":
-		mHash = 0x01D79EDD3081D078
-		pHash = 0x447B8E23EA19AFBF
-		hhKey = 0xC733FDBD9B57119F
-	}
-
-	t.Run("redirected", func(t *testing.T) {
-		proc, err := GetProcAddressByHashML(list, uint(mHash), uint(pHash), uint(hhKey), true)
-		require.NoError(t, err)
-		require.NotZero(t, proc)
-		require.NotEqual(t, VirtualAlloc, proc)
-	})
-
-	t.Run("not redirected", func(t *testing.T) {
-		proc, err := GetProcAddressByHashML(list, uint(mHash), uint(pHash), uint(hhKey), false)
-		require.NoError(t, err)
-		require.Equal(t, VirtualAlloc, proc)
-	})
-
-	t.Run("not found", func(t *testing.T) {
-		proc, err := GetProcAddressByHashML(list, 0x1111, 0x2222, 0x3333, false)
-		require.EqualError(t, err, "failed to call GetProcAddressByHashML: 0xFF000303")
-		require.Zero(t, proc)
-	})
-}
-
-func TestGetProcAddressOriginal(t *testing.T) {
+func TestGetProcAddressRaw(t *testing.T) {
 	libKernel32, err := windows.LoadLibrary("kernel32.dll")
 	require.NoError(t, err)
 	hKernel32 := uintptr(libKernel32)
@@ -195,16 +128,21 @@ func TestGetProcAddressOriginal(t *testing.T) {
 		VirtualAlloc, err := windows.GetProcAddress(libKernel32, "VirtualAlloc")
 		require.NoError(t, err)
 
-		proc, err := GetProcAddressOriginal(hKernel32, "VirtualAlloc")
+		proc, err := GetProcAddressRaw(hKernel32, "VirtualAlloc")
 		require.NoError(t, err)
 		require.Equal(t, VirtualAlloc, proc)
 	})
 
 	t.Run("not found", func(t *testing.T) {
-		proc, err := GetProcAddressOriginal(hKernel32, "NotFound")
-		require.EqualError(t, err, "failed to call GetProcAddressOriginal: 0x0000007F")
+		proc, err := GetProcAddressRaw(hKernel32, "NotFound")
+		require.EqualError(t, err, "failed to call GetProcAddressRaw: 0x0000007F")
 		require.Zero(t, proc)
 	})
+}
+
+func TestGetTEB(t *testing.T) {
+	teb := GetTEB()
+	require.NotZero(t, teb)
 }
 
 func TestGetPEB(t *testing.T) {
@@ -214,13 +152,8 @@ func TestGetPEB(t *testing.T) {
 	require.Equal(t, expect, actual)
 }
 
-func TestGetTEB(t *testing.T) {
-	teb := GetTEB()
-	require.NotZero(t, teb)
-}
-
-func TestGetIMOML(t *testing.T) {
-	actual := GetIMOML()
+func TestGetPML(t *testing.T) {
+	actual := GetPML()
 
 	peb := windows.RtlGetCurrentPeb()
 	addr := uintptr(unsafe.Pointer(peb)) // #nosec
@@ -232,7 +165,8 @@ func TestGetIMOML(t *testing.T) {
 
 		ldr := *(*uintptr)(unsafe.Pointer(addr + 0x0C)) // #nosec
 		mod := *(*uintptr)(unsafe.Pointer(ldr + 0x14))  // #nosec
-		require.Equal(t, mod, actual)
+		pml := mod - 0x08
+		require.Equal(t, pml, actual)
 	})
 
 	t.Run("x64", func(t *testing.T) {
@@ -242,7 +176,8 @@ func TestGetIMOML(t *testing.T) {
 
 		ldr := *(*uintptr)(unsafe.Pointer(addr + 0x18)) // #nosec
 		mod := *(*uintptr)(unsafe.Pointer(ldr + 0x20))  // #nosec
-		require.Equal(t, mod, actual)
+		pml := mod - 0x10
+		require.Equal(t, pml, actual)
 	})
 }
 
