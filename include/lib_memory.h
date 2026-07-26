e27731514ffa5dbf8e670bf2af0b86cc2ac1edb0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-26 13:39:41 +0800

    lib_memory.h: add return value about dst address to methods for write memory.

--- include/lib_memory.h
+++ include/lib_memory.h
@@ -11,17 +11,17 @@ typedef uint  (*msize_t)(void* ptr);
 typedef uint  (*mcap_t)(void* ptr);
 
 // mem_copy is used to copy source memory data to the destination.
-void mem_copy(void* dst, void* src, uint num);
+void* mem_copy(void* dst, void* src, uint num);
 
 // mem_move is used to copy source memory data to the destination.
 // It can handle overlapping memory regions.
-void mem_move(void* dst, void* src, uint num);
+void* mem_move(void* dst, void* src, uint num);
 
 // mem_init is used to fill the memory with zero.
-void mem_init(void* ptr, uint num);
+void* mem_init(void* ptr, uint num);
 
 // mem_set is used to fill the memory with value.
-void mem_set(void* ptr, byte val, uint num);
+void* mem_set(void* ptr, byte val, uint num);
 
 // mem_cmp is used to compare memory data.
 // if a = b, return 0
@@ -29,10 +29,10 @@ void mem_set(void* ptr, byte val, uint num);
 // if a < b, return -1
 int mem_cmp(void* a, void* b, uint num);
 
-// mem_equal is used to compare the memory is equal.
+// mem_equal is used to compare the memory data are equaled.
 bool mem_equal(void* a, void* b, uint num);
 
-// mem_zero is used to check the memory are all zero.
+// mem_is_zero is used to check the memory are all zero.
 bool mem_is_zero(void* ptr, uint num);
 
 #endif // LIB_MEMORY_H
