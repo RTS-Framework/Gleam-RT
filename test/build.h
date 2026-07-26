e0ccd5e10e56243300cdeef461d5e5e48c398e92
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-26 14:01:24 +0800

    build.h: remove the build flag about FAST_CRYPTO, it is deprecated.

--- test/build.h
+++ test/build.h
@@ -3,12 +3,10 @@
 
 // RELEASE_MODE:   remove debug modules for generate template
 // PIC_MODE:       run unit tests under runtime PIC instance
-// FAST_CRYPTO:    replace original algorithm to xor for test
 // NOT_CAMOUFLAGE: disable all modules about camouflage for debug
 
 // #define RELEASE_MODE
 #define PIC_MODE
-// #define FAST_CRYPTO
 // #define NOT_CAMOUFLAGE
 
 #ifdef PIC_MODE
