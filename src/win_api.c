ffb317b7c767cc9a034c6f9f3a4de83742e9bc9f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-27 12:55:55 +0800

    win_api.c: implement GetProcedureName that will use two methods.

--- src/win_api.c
+++ src/win_api.c
@@ -5,7 +5,10 @@
 #include "lib_memory.h"
 #include "lib_string.h"
 #include "hash_api.h"
+#include "pe_image.h"
 #include "win_api.h"
+
+static LPSTR getProcedureName(HMODULE hModule, void* procedure);
 
 BOOL IsValidModuleHandle(PML* pml, HMODULE hModule)
 {
@@ -72,6 +75,76 @@ HMODULE GetModuleHandle(PML* pml, LPWSTR lpBasename)
         if (strnicmp_w(lpBasename, nameBuf, nameLen) == 0)
         {
             return entry->ImageBase;
+        }
+    }
+    return NULL;
+}
+
+LPSTR GetProcedureName(PML* pml, HMODULE hModule, void* procedure)
+{
+    if (procedure == NULL)
+    {
+        return NULL;
+    }
+    // try to get procedure name from self module first
+    if (hModule != NULL)
+    {
+        LPSTR name = getProcedureName(hModule, procedure);
+        if (name != NULL)
+        {
+            return name;
+        }
+    }
+    // if not exist, search procedure name from pml.
+    // like forwarded procedure in EAT.
+    LIST_ENTRY* head = &pml->Links;
+    for (LIST_ENTRY* link = head->Flink; link != head; link = link->Flink)
+    {
+        PML* entry = (PML*)((uintptr)(link)-offsetof(PML, Links));
+        LPSTR name = getProcedureName(entry->ImageBase, procedure);
+        if (name != NULL)
+        {
+            return name;
+        }
+    }
+    return NULL;
+}
+
+static LPSTR getProcedureName(HMODULE hModule, void* procedure)
+{
+    uintptr proc = (uintptr)procedure;
+    // check image base address is expected
+    uintptr base = (uintptr)(hModule);
+    // parse module information
+    PE_Image image;
+    mem_init(&image, sizeof(image));
+    ParsePEImage(hModule, &image);
+    // calculate the text section range
+    uintptr begin = base + image.Text.VirtualAddress;
+    uintptr end = begin + image.Text.VirtualSize;
+    if (proc < begin || proc > end)
+    {
+        return NULL;
+    }
+    // get RVA of export address tables(EAT)
+    Image_DataDirectory* DD = &image.OptionalHeader.DataDirectory[0];
+    Image_DataDirectory  EAT = DD[IMAGE_DIRECTORY_ENTRY_EXPORT];
+    // get export directory structure
+    Image_ExportDirectory* dir = (Image_ExportDirectory*)(base + EAT.VirtualAddress);
+    // process EAT arrays
+    uint32* nameTable = (uint32*)(base + dir->AddressOfNames);
+    uint32* funcTable = (uint32*)(base + dir->AddressOfFunctions);
+    uint16* ordiTable = (uint16*)(base + dir->AddressOfNameOrdinals);
+    // compare the exported function address
+    for (uint32 i = 0; i < dir->NumberOfNames; i++)
+    {
+        // name[i] -> ordinal[i] -> funcRVA[ordinal]
+        uint32 funcRVA = funcTable[ordiTable[i]];
+        if (base + funcRVA == proc)
+        {
+            // get procedure name address
+            byte* procName = (byte*)(base + nameTable[i]);
+            return procName;
         }
     }
     return NULL;
